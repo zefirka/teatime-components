9869b0ee910fe148ae2146f66932e877ebf7ddc8
Alexey Litvinov <sullenor@yandex-team.ru>
2016-08-19 10:42:38 +0300

    simple button implementation

--- component/Button.js
+++ component/Button.js
@@ -1,56 +1,79 @@
 'use strict';
 
-const { PropTypes } = require('react');
-const Button = require('../view/Button');
+const { Component, PropTypes } = require('react');
+const { curry, get, isUndefined } = require('lodash/fp');
+const React = require('react');
+const classNames = require('classnames');
 
-const predefinedStyles = {
-  'action-xs': require('../style/button/button-action-xs.css'),
-  'action-s': require('../style/button/button-action-s.css'),
-  'action-m': require('../style/button/button-action-m.css'),
-  'action-l': require('../style/button/button-action-l.css'),
-  'link-xs': require('../style/button/button-link-xs.css'),
-  'link-s': require('../style/button/button-link-s.css'),
-  'link-m': require('../style/button/button-link-m.css'),
-  'link-l': require('../style/button/button-link-l.css'),
-  'normal-xs': require('../style/button/button-normal-xs.css'),
-  'normal-s': require('../style/button/button-normal-s.css'),
-  'normal-m': require('../style/button/button-normal-m.css'),
-  'normal-l': require('../style/button/button-normal-l.css'),
+const defined = {
+  'l-action': require('../style/button/button-action-l.css'),
+  'l-link': require('../style/button/button-link-l.css'),
+  'l-normal': require('../style/button/button-normal-l.css'),
+  'm-action': require('../style/button/button-action-m.css'),
+  'm-link': require('../style/button/button-link-m.css'),
+  'm-normal': require('../style/button/button-normal-m.css'),
+  's-action': require('../style/button/button-action-s.css'),
+  's-link': require('../style/button/button-link-s.css'),
+  's-normal': require('../style/button/button-normal-s.css'),
+  'xs-action': require('../style/button/button-action-xs.css'),
+  'xs-link': require('../style/button/button-link-xs.css'),
+  'xs-normal': require('../style/button/button-normal-xs.css'),
 };
 
-class ButtonComponent extends Button {
-  /**
-   * @return {object}
-   */
-  styles() {
-    return predefinedStyles[this.props.theme + '-' + this.props.size];
+// @todo make common for all controls and put to the standalone module
+// possible syntax:
+// ```
+// const style = require('./style')(defined, getter);
+// style(size, theme, styles)(styleName)
+// ```
+const style = curry((defined, size, theme, styles, styleName) =>
+  get(styleName, isUndefined(styles) ? defined[size + '-' + theme] : styles))(defined);
+
+class Button extends Component {
+  focus() {
+    if (this.refs.control) {
+      this.refs.control.focus();
+    }
+  }
+
+  render() {
+    const {
+      className,
+      size,
+      styles,
+      theme,
+      ...other,
+    } = this.props;
+
+    return (
+      <button
+        {...other}
+        className={classNames(style(size, theme, styles, 'control'), className)}
+        ref='control'/>
+    );
   }
 }
 
-ButtonComponent.defaultProps = {
+Button.defaultProps = {
   size: 's',
   theme: 'normal',
-  ...Button.defaultProps,
 };
 
-ButtonComponent.propTypes = {
+Button.propTypes = {
   size: PropTypes.oneOf([
-    'xs',
-    's',
-    'm',
     'l',
+    'm',
+    's',
+    'xs',
   ]),
+  styles: PropTypes.shape({
+    control: PropTypes.string.isRequired,
+  }),
   theme: PropTypes.oneOf([
     'action',
     'link',
     'normal',
   ]),
-  ...Button.propTypes,
 };
 
-ButtonComponent.unwantedProps = [
-  'size',
-  'theme',
-];
-
-module.exports = ButtonComponent;
+module.exports = Button;
