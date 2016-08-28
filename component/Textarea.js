5bfb316602b6abb2aa404c7de6e04304f152f150
Alexey Litvinov <sullenor@yandex-team.ru>
2016-08-28 18:43:52 +0300

    Textarea

--- component/Textarea.js
+++ component/Textarea.js
@@ -1,37 +1,66 @@
 'use strict';
 
-const { PropTypes } = require('react');
-const Textarea = require('../view/Textarea');
+const { Component, PropTypes } = require('react');
+const { identity, noop } = require('lodash/fp');
+const React = require('react');
+const classNames = require('classnames');
+const style = require('../tool/style');
 
-const predefinedStyles = {
+const cn = style(identity, {
   s: require('../style/textarea/textarea-s.css'),
   m: require('../style/textarea/textarea-m.css'),
-};
+});
 
-class TextareaComponent extends Textarea {
-  /**
-   * @return {object}
-   */
-  styles() {
-    return predefinedStyles[this.props.size];
+class Textarea extends Component {
+  focus() {
+    if (!this.refs.textarea) return;
+    this.refs.textarea.focus();
+  }
+
+  select() {
+    if (!this.refs.textarea) return;
+    this.refs.textarea.select();
+  }
+
+  onChange(e) {
+    this.props.onChange(e, {value: e.target.value});
+  }
+
+  render() {
+    const {
+      className,
+      size,
+      styles,
+      ...other,
+    } = this.props;
+
+    return (
+      <textarea
+        {...other}
+        className={classNames(cn(styles, 'control', size), className)}
+        ref='textarea'/>
+    );
   }
 }
 
-TextareaComponent.defaultProps = {
+Textarea.defaultProps = {
+  onChange: noop,
   size: 's',
-  ...Textarea.defaultProps,
+  rows: 3,
 };
 
-TextareaComponent.propTypes = {
+Textarea.propTypes = {
+  cols: PropTypes.number,
+  rows: PropTypes.number,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
   size: PropTypes.oneOf([
     's',
     'm',
   ]),
-  ...Textarea.propTypes,
+  styles: PropTypes.shape({
+    control: PropTypes.string.isRequired,
+  }),
 };
 
-TextareaComponent.unwantedProps = [
-  'size',
-];
-
-module.exports = TextareaComponent;
+module.exports = Textarea;
